1a9353c81409608b01117dce40325dbe2e0d380d
verronyvl <80186678+verronyvl@users.noreply.github.com>
2021-03-09 10:09:29 +1000

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -20,14 +20,14 @@ rkt = ['falcon1', 'falcon9', 'falconheavy']
 for i in rkt:
     t1 = BashOperator(
         task_id="get_data_"+i, 
-        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -r i -o /var/data/"+i, 
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -r i -o /var/data/", 
         dag=dag
     )
     
     t2 = BashOperator(
         task_id="print_data_"+i, 
-        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-        params={"rocket": i}, # falcon1/falcon9/falconheavy
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={}/data.csv".format(i), 
+        #params={"rocket": i}, # falcon1/falcon9/falconheavy
         dag=dag
     )
     
